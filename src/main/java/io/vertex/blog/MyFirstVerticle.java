8fa9516e3c844fcebb0c06002add3db5248489ac
bikash1605 <bikash@mediaiqdigital.com>
2016-11-24 17:32:40 +0530

    first commit

---
+++ src/main/java/io/vertex/blog/MyFirstVerticle.java
@@ -0,0 +1,26 @@
+package io.vertex.blog;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+public class MyFirstVerticle extends AbstractVerticle{
+
+	@Override
+	public void start(Future<Void> future) throws Exception {
+		vertx
+			.createHttpServer()
+			.requestHandler(r -> {
+				r.response().end("</h1>Hello from my first " + 
+						"Vert.x 3 application</h1>");
+			})
+			.listen(8080, result -> {
+				if(result.succeeded()) {
+					future.complete();
+				}else{
+					future.fail(result.cause());
+				}
+			});
+			
+	}
+
+}
